adcee41dc1c6c21a343633f4e489e453a5315c03
Kele Huang <rivalak@163.com>
2019-09-24 15:01:07 +0800

    1 master boot record (MBR) finished 2 delete Chap_04 3 add Chap_05/README.md & update .gitignore 4 keep the style of Code_5/Makefile as before

--- Chap_05/CheckP_5-4/5-4.asm
+++ Chap_05/CheckP_5-4/5-4.asm
@@ -1,7 +1,10 @@
-;author: rivalak
-;Time: 2019-03-22 21:00
+            ; author: rivalak
+            ; Time: 2019-03-22 21:00
+            ; P66
 
-            jmp near start
-    data    db 0x55, 0xaa
+            ; 通过反汇编验证两条 jmp (相对近跳转和绝对地址跳转) 的机器码
+
+            jmp near start              ; E9 02 00   (5 - 3)
+    data:    db 0x55, 0xaa               ; 注：两字节长度
     start:  mov ax, 0
-            jmp 0x2000:0x0005
+            jmp 0x2000:0x0005           ; EA 05 00 00 20
--- Chap_05/Code_5/asm/c_5.asm
+++ Chap_05/Code_5/asm/c_5.asm
@@ -1,19 +1,19 @@
-        ;Code of Chapter 5
-        ;P46
-        ;File name: c_5.asm
-        ;Description: Get familiar with MBR(Master Boot Record)
-        ;Author: rivalak
-        ;Time: 2019-03-25 10:32
+  ; Code of Chapter 5
+  ; P46
+  ; Author: rivalak
+  ; Time: 2019-03-25 10:32
 
+  ; Description: Get familiar with MBR(Master Boot Record)
+  ; 显示 Label offset:[number]
+
+  _code_segment:
         mov ax, 0xb800
-        mov es, ax                      ;The begin physic address of graphics memory (project)
+        mov es, ax                       ; The begin physic address of graphics memory (project)
 
-        mov cx, cs
-        mov ds, cx                      ;cs = ds cause the data(number) in the same segment as code
 
-        ;(graphic memory)
-        ;Display string "Label offset:"
-        ;ASCII(1 Byte) + Attributes(1 Byte)
+                                         ; (graphic memory)
+                                         ; Display string "Label offset:"
+                                         ; ASCII(1 Byte) + Attributes(1 Byte)
         mov byte [es:0x00], 'L'
         mov byte [es:0x01], 0x07
         mov byte [es:0x02], 'a'
@@ -41,68 +41,75 @@
         mov byte [es:0x18], ':'
         mov byte [es:0x19], 0x07
 
-        mov ax, number                  ;Dividend: dx:ax ; number replace the data address
-        mov bx, 10                      ;Divisor: bx
+        ; Dividend: dx:ax ; number replace the data address
+        ; Divisor: bx
+				; number 是一个标号，存的是其汇编地址
+        mov ax, number
+        mov bx, 10
 
-        ;(main memory)
-        ;ds = 0x0000, the code being loaded at 0x7c00 
-        ;mov dx, 0                       ;3 Bytes
-        xor dx, dx                      ;2 Bytes
+        mov cx, cs
+        mov ds, cx                       ; cs = ds cause the data(number) in the same segment as code
+                                         ; (main memory)
+                                         ; ds = 0x0000, the code being loaded at 0x7c00
+                                         ; mov dx, 0                       ;3 Bytes
+        xor dx, dx                       ; 2 Bytes
         div bx
-        mov [0x7c00+number+0x00], dl    ;single
-
-        xor dx, dx                      
-        div bx
-        mov [0x7c00+number+0x01], dl    ;tens
+        mov [0x7c00+number+0x00], dl     ; single
 
         xor dx, dx
         div bx
-        mov [0x7c00+number+0x02], dl    ;hundreds
+        mov [0x7c00+number+0x01], dl     ; tens
 
         xor dx, dx
         div bx
-        mov [0x7c00+number+0x03], dl    ;thousands
+        mov [0x7c00+number+0x02], dl     ; hundreds
 
         xor dx, dx
         div bx
-        mov [0x7c00+number+0x04], dl    ;ten thousands
+        mov [0x7c00+number+0x03], dl     ; thousands
 
-        ;get ASCII code by add 0x30
-        mov al, [0x7c00+number+0x04]
+        xor dx, dx
+        div bx
+        mov [0x7c00+number+0x04], dl     ; ten thousands
+
+                                         ; get ASCII code by add 0x30
+                                         ; 0x04 是黑底红字； 0x07 黑底白字
+                mov al, [0x7c00+number+0x04]
         add al, 0x30
         mov [es:0x1a], al
-        mov byte [es:0x1b], 0x04        ;black ground red fore
+        mov byte [es:0x1b], 0x04
 
         mov al, [0x7c00+number+0x03]
         add al, 0x30
         mov [es:0x1c], al
-        mov byte [es:0x1d], 0x04        ;black ground red fore
+        mov byte [es:0x1d], 0x04
 
         mov al, [0x7c00+number+0x02]
         add al, 0x30
         mov [es:0x1e], al
-        mov byte [es:0x1f], 0x04        ;black ground red fore
+        mov byte [es:0x1f], 0x04
 
         mov al, [0x7c00+number+0x01]
         add al, 0x30
         mov [es:0x20], al
-        mov byte [es:0x21], 0x04        ;black ground red fore
+        mov byte [es:0x21], 0x04
 
         mov al, [0x7c00+number+0x00]
         add al, 0x30
         mov [es:0x22], al
-        mov byte [es:0x23], 0x04        ;black ground red fore
+        mov byte [es:0x23], 0x04
 
         mov byte [es:0x24], 'D'
         mov byte [es:0x25], 0x07
 
-        ;infinit loop
+        ; infinit loop
     infi:
         jmp near infi
 
-    number: 
+    _data_segment:
+    number:
         db 0, 0, 0, 0, 0
 
-    times 204 db 0                  ;Padding
-              db 0x55, 0xaa         ;Effective flag of MBR
+    times 204 db 0                       ; Padding
+              db 0x55, 0xaa              ; Effective flag of MBR
 
