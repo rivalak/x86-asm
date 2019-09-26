1cf4d8c82e1a86703a7e647d9b7e27420c5bb2ef
Kele Huang <rivalak@163.com>
2019-09-26 17:35:46 +0800

    1 simplify Chap_07/Makefile

--- Chap_07/asm/c_07.asm
+++ Chap_07/asm/c_07.asm
@@ -1,9 +1,7 @@
-        ;code of Chapter 7
-        ;P98
-        ;File name: c_07.asm
-        ;Description: Get familiar with 'Stack'
-        ;Author: rivalak
-        ;Time: 2019-03-28 17:04
+        ; code of Chapter 7  P98
+        ; Author: rivalak
+        ; Time: 2019-03-28 17:04
+        ; Description: to get familiar with 'Stack'
 
         jmp near start
 
@@ -16,10 +14,10 @@ start:
         mov ax,0xb800
         mov es,ax
 
-        ;show "message"
+        ; show "message"
         mov si,message
         mov di,0
-        mov cx,start-message
+        mov cx,start - message
     @show_mess:
         mov al,[si]
         mov [es:di],al
@@ -29,7 +27,7 @@ start:
         inc si
         loop @show_mess
 
-        ;get answer
+        ; get answer
         xor ax,ax
         mov cx,100
     @get_answ:
@@ -38,10 +36,10 @@ start:
         cmp cx,0
         jne @get_answ
 
-        ;use Stack to store 'answer'
+        ; use Stack to store 'answer'
         xor cx,cx
-        mov ss,cx               ;set stack segment address as 0
-        mov sp,cx               ;set stack pointer address as 0
+        mov ss,cx       ; set stack segment address as 0
+        mov sp,cx       ; set stack pointer address as 0
 
         mov bx,10
     @stac_answ:
@@ -49,24 +47,24 @@ start:
         xor dx,dx
         div bx
         or dl,0x30
-        push dx                 ;only support word
+        push dx         ; only support word
         cmp ax,0
         jne @stac_answ
 
-        ;show 'answer'
+        ; show 'answer'
     @show_answ:
         pop dx
         mov [es:di],dl
         inc di
         mov byte [es:di],0x07
         inc di
-        loop @show_answ         ;loop auto do cx-1
+        loop @show_answ ; loop auto do cx-1
 
-        jmp near $              ;infinit loop
+        jmp near $      ; infinit loop
 
 times 510-($-$$) db 0
-                 dw 0xaa55      
-        
+                 dw 0xaa55
 
-        
-        
+
+
+
--- Chap_08/asm/c08.asm
+++ Chap_08/asm/c08.asm
@@ -1,45 +1,42 @@
-        ;Code of Chapter 8
-        ;P114
-        ;File name: c08.asm
-        ;Description: user program
-        ;Author: rivalak
-        ;Create Time: 2019-03-29 19:18
+; Code of Chapter 8
+; P114
+; File name: c08.asm
+; Description: user program
+; Author: rivalak
+; Create Time: 2019-03-29 19:18
+; ================================================================================
+SECTION header vstart=0                                 ; define user program header segment
+    program_length  dd program_end
 
-;================================================================================
-SECTION header vstart=0                             ;define user program header segment
-    program_length  dd program_end                   ;[0x00] length of program
+    code_entry      dw start                            ; 入口点的偏移地址
+                    dd section.code_1.start             ; 入口点的段地址
 
-    code_entry      dw start                        ;[0x04] offset address
-                    dd section.code_1.start         ;[0x06] segment address
+    realloc_tbl_len dw (header_end - code_1_segment)/4  ; 表项数
 
-    realloc_tbl_len dw (header_end - code_1_segment)/4
-                                                    ;[0x0a] number of segment realloc
-
-    ;segment realloc table
-    code_1_segment  dd section.code_1.start         ;[0x0c]
-    code_2_segment  dd section.code_2.start         ;[0x10]
-    data_1_segment  dd section.data_1.start         ;[0x14]
-    data_2_segment  dd section.data_2.start         ;[0x18]
-    stack_segment   dd section.stack.start          ;[0x1c]
+    code_1_segment  dd section.code_1.start             ; segment realloc table
+    code_2_segment  dd section.code_2.start
+    data_1_segment  dd section.data_1.start
+    data_2_segment  dd section.data_2.start
+    stack_segment   dd section.stack.start
 
     header_end:
-;===============================================================================
-SECTION code_1 align=16 vstart=0         ;定义代码段1（16字节对齐） 
-put_string:                              ;显示串(0结尾)。
-                                         ;输入：DS:BX=串地址
+    ; ===============================================================================
+SECTION code_1 align=16 vstart=0                        ; 定义代码段1（16字节对齐）
+put_string:                                             ; 显示串(0结尾)。
+                                                        ; 输入：DS:BX=串地址
          mov cl,[bx]
-         or cl,cl                        ;cl=0 ?
-         jz .exit                        ;是的，返回主程序 
+         or cl,cl                                       ; cl=0 ?
+         jz .exit                                       ; 是的，返回主程序
          call put_char
-         inc bx                          ;下一个字符 
+         inc bx                                         ; 下一个字符
          jmp put_string
 
    .exit:
          ret
 
-;-------------------------------------------------------------------------------
-put_char:                                ;显示一个字符
-                                         ;输入：cl=字符ascii
+         ; -------------------------------------------------------------------------------
+put_char:                                               ; 显示一个字符
+                                                        ; 输入：cl=字符ascii
          push ax
          push bx
          push cx
@@ -47,48 +44,48 @@ put_char:                                ;显示一个字符
          push ds
          push es
 
-         ;以下取当前光标位置
+         ; 以下取当前光标位置
          mov dx,0x3d4
          mov al,0x0e
          out dx,al
          mov dx,0x3d5
-         in al,dx                        ;高8位 
+         in al,dx                                       ; 高8位
          mov ah,al
 
          mov dx,0x3d4
          mov al,0x0f
          out dx,al
          mov dx,0x3d5
-         in al,dx                        ;低8位 
-         mov bx,ax                       ;BX=代表光标位置的16位数
+         in al,dx                                       ; 低8位
+         mov bx,ax                                      ; BX=代表光标位置的16位数
 
-         cmp cl,0x0d                     ;回车符？
-         jnz .put_0a                     ;不是。看看是不是换行等字符 
-         mov ax,bx                       ;此句略显多余，但去掉后还得改书，麻烦 
-         mov bl,80                       
+         cmp cl,0x0d                                    ; 回车符？
+         jnz .put_0a                                    ; 不是。看看是不是换行等字符
+         mov ax,bx                                      ; 此句略显多余，但去掉后还得改书，麻烦
+         mov bl,80
          div bl
          mul bl
          mov bx,ax
          jmp .set_cursor
 
  .put_0a:
-         cmp cl,0x0a                     ;换行符？
-         jnz .put_other                  ;不是，那就正常显示字符 
+         cmp cl,0x0a                                    ; 换行符？
+         jnz .put_other                                 ; 不是，那就正常显示字符
          add bx,80
          jmp .roll_screen
 
- .put_other:                             ;正常显示字符
+ .put_other:                                            ; 正常显示字符
          mov ax,0xb800
          mov es,ax
          shl bx,1
          mov [es:bx],cl
 
-         ;以下将光标位置推进一个字符
+         ; 以下将光标位置推进一个字符
          shr bx,1
          add bx,1
 
  .roll_screen:
-         cmp bx,2000                     ;光标超出屏幕？滚屏
+         cmp bx,2000                                    ; 光标超出屏幕？滚屏
          jl .set_cursor
 
          mov ax,0xb800
@@ -99,7 +96,7 @@ put_char:                                ;显示一个字符
          mov di,0x00
          mov cx,1920
          rep movsw
-         mov bx,3840                     ;清除屏幕最底一行
+         mov bx,3840                                    ; 清除屏幕最底一行
          mov cx,80
  .cls:
          mov word[es:bx],0x0720
@@ -131,45 +128,45 @@ put_char:                                ;显示一个字符
 
          ret
 
-;-------------------------------------------------------------------------------
+         ; -------------------------------------------------------------------------------
   start:
-         ;初始执行时，DS和ES指向用户程序头部段
-         mov ax,[stack_segment]           ;设置到用户程序自己的堆栈 
+         ; 初始执行时，DS和ES指向用户程序头部段
+         mov ax,[stack_segment]                         ; 设置到用户程序自己的堆栈
          mov ss,ax
          mov sp,stack_end
-         
-         mov ax,[data_1_segment]          ;设置到用户程序自己的数据段
+
+         mov ax,[data_1_segment]                        ; 设置到用户程序自己的数据段
          mov ds,ax
 
          mov bx,msg0
-         call put_string                  ;显示第一段信息 
+         call put_string                                ; 显示第一段信息
 
          push word [es:code_2_segment]
          mov ax,begin
-         push ax                          ;可以直接push begin,80386+
-         
-         retf                             ;转移到代码段2执行 
-         
+         push ax                                        ; 可以直接push begin,80386+
+
+         retf                                           ; 转移到代码段2执行
+
   continue:
-         mov ax,[es:data_2_segment]       ;段寄存器DS切换到数据段2 
+         mov ax,[es:data_2_segment]                     ; 段寄存器DS切换到数据段2
          mov ds,ax
-         
+
          mov bx,msg1
-         call put_string                  ;显示第二段信息 
+         call put_string                                ; 显示第二段信息
 
-         jmp $ 
+         jmp $
 
-;===============================================================================
-SECTION code_2 align=16 vstart=0          ;定义代码段2（16字节对齐）
+         ; ===============================================================================
+SECTION code_2 align=16 vstart=0                        ; 定义代码段2（16字节对齐）
 
   begin:
          push word [es:code_1_segment]
          mov ax,continue
-         push ax                          ;可以直接push continue,80386+
-         
-         retf                             ;转移到代码段1接着执行 
-         
-;===============================================================================
+         push ax                                        ; 可以直接push continue,80386+
+
+         retf                                           ; 转移到代码段1接着执行
+
+                                                        ; ===============================================================================
 SECTION data_1 align=16 vstart=0
 
     msg0 db '  This is NASM - the famous Netwide Assembler. '
@@ -190,21 +187,21 @@ SECTION data_1 align=16 vstart=0
          db '     ... ...(Some other codes)',0x0d,0x0a,0x0d,0x0a
          db 0
 
-;===============================================================================
+         ; ===============================================================================
 SECTION data_2 align=16 vstart=0
 
     msg1 db '  The above contents is written by LeeChung. '
          db '2011-05-06'
          db 0
 
-;===============================================================================
+         ; ===============================================================================
 SECTION stack align=16 vstart=0
-           
+
          resb 256
 
-stack_end:  
+stack_end:
 
-;===============================================================================
+; ===============================================================================
 SECTION trail align=16
 program_end:
 
